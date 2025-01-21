3069dabd3c4706b444d70e080bab06f7614f65b2
Andrew Parmar <andrew.parmar@pivotal.aero>
2025-01-21 00:59:16 -0800

    Tweaking configs to enable debugging in dev-tools: needs work

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // For best debugging, you can try "eval-source-map" or "inline-source-map"
+      config.devtool = 'eval-source-map';
+    }
+    return config;
+  },
+  output: 'export',
 };
 
 export default nextConfig;
