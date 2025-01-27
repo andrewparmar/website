9de95825aedb9d0eca3f0e45d0601b068e1025fb
Andrew Parmar <andrew.parmar@pivotal.aero>
2025-01-26 23:31:55 -0800

    Use out directory

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      // For best debugging, you can try "eval-source-map" or "inline-source-map"
-      config.devtool = 'eval-source-map';
-    }
-    return config;
-  },
   output: 'export',
-  distDir: 'docs',
+  // distDir: 'docs',
   basePath: '/website',
   assetPrefix: '/website/',
   trailingSlash: true
